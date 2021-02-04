c6e2e4d4a91989f56d9b74ab96e366adb9ab7043
akomissarova <akomissarovaaaa@gmail.com>
2021-02-04 19:08:42 +0100

    Take SOAP API URL from target.json

--- fixture/soap.py
+++ fixture/soap.py
@@ -7,9 +7,10 @@ class SoapHelper:
 
     def __init__(self, app):
         self.app = app
+        self.soap_url = app.base_url + "api/soap/mantisconnect.php?wsdl"
 
     def can_login(self, user, password):
-        client = Client("http://192.168.64.2/mantisbt/api/soap/mantisconnect.php?wsdl")
+        client = Client(self.soap_url)
         try:
             client.service.mc_login(user, password)
             return True
@@ -17,7 +18,7 @@ class SoapHelper:
             return False
 
     def get_projects_list_for_user(self, user, password):
-        client = Client("http://192.168.64.2/mantisbt/api/soap/mantisconnect.php?wsdl")
+        client = Client(self.soap_url)
         try:
             soap_projects = client.service.mc_projects_get_user_accessible(user, password)
         except WebFault:
